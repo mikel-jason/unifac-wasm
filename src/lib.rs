e9716670bb98ffa92d85f29b8466842019d2f3ba
Mikel Muennekhoff <mmuennekhoff+github@gmail.com>
2021-02-02 10:17:36 +0100

    Port Rust to wasm
    - Building adapter crate
    - Requires wasm-bindgen ^0.2.70 due to bug in toolchain [1]
    - Requires local installation of sarcaustech/unifac [2] with no
      parallelization (Rayon). Used with commit 4711916
    
    [1] https://github.com/rustwasm/wasm-pack/issues/886
    [2] https://github.com/sarcaustech/unifac

---
+++ src/lib.rs
@@ -0,0 +1,83 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+#[derive(Debug, Clone)]
+pub struct Substance {
+    pub fraction: f64,
+    pub gamma: Option<f64>,
+    functional_groups: Vec<unifac::FunctionalGroup>,
+}
+
+#[wasm_bindgen]
+#[derive(Debug, Clone)]
+pub struct Mixture {
+    pub temperature: f64,
+    substances: Vec<unifac::Substance>,
+}
+
+#[wasm_bindgen]
+impl Mixture {
+    #[wasm_bindgen(constructor)]
+    pub fn new(temperature: f64) -> Self {
+        Self {
+            temperature,
+            substances: Vec::new(),
+        }
+    }
+
+    #[wasm_bindgen]
+    pub fn add_substance(&mut self, substance: Substance) -> Self {
+        let s = unifac::Substance::from(substance.fraction, substance.functional_groups);
+        self.substances.push(s);
+        self.clone()
+    }
+
+    #[wasm_bindgen]
+    pub fn print(&self) -> js_sys::JsString {
+        js_sys::JsString::from(format!("{:?}", self)).clone()
+    }
+
+    #[wasm_bindgen]
+    pub fn calc(&self) -> js_sys::Array {
+        match unifac::calc(self.substances.clone(), self.temperature) {
+            Ok(res) => {
+                let arr = js_sys::Array::new();
+                for s in res {
+                    arr.push(&JsValue::from_f64(s.gamma.unwrap()));
+                }
+                arr
+            }
+            Err(_) => js_sys::Array::new(),
+        }
+    }
+}
+
+#[wasm_bindgen]
+impl Substance {
+    #[wasm_bindgen(constructor)]
+    pub fn new(fraction: f64) -> Self {
+        Substance {
+            fraction,
+            functional_groups: Vec::new(),
+            gamma: None,
+        }
+    }
+
+    #[wasm_bindgen]
+    pub fn add_functional_group(&mut self, id: u8, nu: f64) -> Self {
+        let fg = unifac::FunctionalGroup::from(id, nu).expect("FG add_functional_group");
+        self.functional_groups.push(fg);
+        self.clone()
+    }
+
+    #[wasm_bindgen]
+    pub fn print(&self) -> js_sys::JsString {
+        let s: Vec<String> = self
+            .functional_groups
+            .iter()
+            .map(|fg| format!("{} ({} times)", fg.id, fg.nu))
+            .collect();
+        let joined = s.join(" ");
+        js_sys::JsString::from(joined).clone()
+    }
+}
