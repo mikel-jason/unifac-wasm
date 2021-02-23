4b351f7fb033b2421b010b87b0da4d1285b98a51
Mikel Muennekhoff <mmuennekhoff+github@gmail.com>
2021-02-23 10:58:24 +0100

    Remove unused code from example

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const button = document.getElementById('render');
-const canvas = document.getElementById('canvas');
-const scene = document.getElementById('scene');
 const concurrency = document.getElementById('concurrency');
 const concurrencyAmt = document.getElementById('concurrency-amt');
-const timing = document.getElementById('timing');
-const timingVal = document.getElementById('timing-val');
-const ctx = canvas.getContext('2d');
-
 button.disabled = true;
 concurrency.disabled = true;
 
@@ -33,29 +27,13 @@ function loadWasm() {
 
 loadWasm();
 
-const { Scene, WorkerPool } = wasm_bindgen;
+const { WorkerPool, unifac_test } = wasm_bindgen;
 
 function run() {
-  // The maximal concurrency of our web worker pool is `hardwareConcurrency`,
-  // so set that up here and this ideally is the only location we create web
-  // workers.
   pool = new WorkerPool(navigator.hardwareConcurrency);
 
   // Configure various buttons and such.
-  button.onclick = function() {
-    button.disabled = true;
-    console.time('render');
-    let json;
-    try {
-      json = JSON.parse(scene.value);
-    } catch(e) {
-      alert(`invalid json: ${e}`);
-      return
-    }
-    canvas.width = json.width;
-    canvas.height = json.height;
-    render(new Scene(json));
-  };
+  button.onclick = run_wasm
   button.innerText = 'Render!';
   button.disabled = false;
 
@@ -70,58 +48,6 @@ function run() {
   concurrency.disabled = false;
 }
 
-let rendering = null;
-let start = null;
-let interval = null;
-let pool = null;
-
-class State {
-  constructor(wasm) {
-    this.start = performance.now();
-    this.wasm = wasm;
-    this.running = true;
-    this.counter = 1;
-
-    this.interval = setInterval(() => this.updateTimer(true), 100);
-
-    wasm.promise()
-      .then(data => {
-        this.updateTimer(false);
-        this.updateImage(data);
-        this.stop();
-      })
-      .catch(console.error);
-  }
-
-  updateTimer(updateImage) {
-    const dur = performance.now() - this.start;
-    timingVal.innerText = `${dur}ms`;
-    this.counter += 1;
-
-    if (updateImage && this.wasm && this.counter % 3 == 0)
-      this.updateImage(this.wasm.imageSoFar());
-  }
-
-  updateImage(data) {
-    ctx.putImageData(data, 0, 0);
-  }
-
-  stop() {
-    if (!this.running)
-      return;
-    console.timeEnd('render');
-    this.running = false;
-    this.wasm = null;
-    clearInterval(this.interval);
-    button.disabled = false;
-  }
-}
-
-function render(scene) {
-  if (rendering) {
-    rendering.stop();
-    rendering = null;
-  }
-  // rendering = new State(scene.render(parseInt(concurrency.value), pool));
-  rendering = scene.unifac_test(parseInt(concurrency.value) , pool)
+function run_wasm() {
+  unifac_test(parseInt(concurrency.value) , pool)
 }
