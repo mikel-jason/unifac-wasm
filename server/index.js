58167de982cd1d0bf21ab555c7d4f2096a1ac654
Mikel Muennekhoff <>
2021-06-01 11:35:17 +0200

    Make no. of runs adjustable

--- server/index.js
+++ server/index.js
@@ -68,6 +68,14 @@ const click = function () {
 const click_measurement = function () {
     clearResults()
 
+    let runs = parseInt(document.getElementById('runs').value)
+    console.log("runs: " + runs)
+
+    if(runs === NaN || runs === undefined || runs <= 0) {
+        console.error("Invalid number of runs: " + runs)
+        return;
+    }
+
     let content = document.getElementById('yml').value
     let yml = yaml.load(content)
     let k = 0 
@@ -94,7 +102,7 @@ const click_measurement = function () {
         }
         console.log("Setup finished");
 
-        for (k = 0; k < 1000; k++) {
+        for (k = 0; k < runs; k++) {
             let start = performance.now()
             for (j = 0; j < 10000; j++) {
                 let res = mixes[j].calc()
